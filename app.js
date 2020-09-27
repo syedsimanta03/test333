eb444ee4370ac2721551c2b564943127eeae293a
sam <syed.simanta06@gmail.com>
2020-09-27 06:05:24 +0600

    added

--- app.js
+++ app.js
@@ -70,6 +70,6 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 4000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
